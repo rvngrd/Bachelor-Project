cbdaeff9f7f459a3f72d25b6eba4db123f604f1f
rvngrd <rrvngrdd@gmail.com>
2024-05-03 22:26:44 +0330

    Finishing part 42(Displaying products details(get_object_or_404))

--- product_module/views.py
+++ product_module/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.http import Http404
 # Create your views here.
 
 
@@ -7,4 +8,15 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_module/product_list.html', context={
         'products': products
+    })
+
+
+def product_detail(request, product_id):
+    # try:
+    #     product = Product.objects.get(id=product_id)
+    # except:
+    #     raise Http404
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product_module/product_detail.html', context={
+        'product': product
     })
