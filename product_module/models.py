6dfc2a12bf169506a5fc3c3a0bed254f39a79d78
rvngrd <rrvngrdd@gmail.com>
2024-05-03 11:36:42 +0330

    Finishing part 37(adding new fields and using __str__)

--- product_module/models.py
+++ product_module/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -6,3 +7,9 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=300)
     price = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=1)
+    short_description = models.CharField(max_length=360, null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
