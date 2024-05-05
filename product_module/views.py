5568f363d8b97e478aa25950180806a27a520de9
rvngrd <rrvngrdd@gmail.com>
2024-05-05 13:10:17 +0330

    Finishing part 45(Slug in url and views, and db_index)

--- product_module/views.py
+++ product_module/views.py
@@ -11,12 +11,12 @@ def product_list(request):
     })
 
 
-def product_detail(request, product_id):
+def product_detail(request, slug):
     # try:
     #     product = Product.objects.get(id=product_id)
     # except:
     #     raise Http404
-    product = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, slug=slug)
     return render(request, 'product_module/product_detail.html', context={
         'product': product
     })
