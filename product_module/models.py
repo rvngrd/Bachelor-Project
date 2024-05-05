5f265a1d276e18444fee64a69f7135bcf65c36f2
rvngrd <rrvngrdd@gmail.com>
2024-05-05 19:51:47 +0330

    Finishing part 51(One to many relation, adding category)

--- product_module/models.py
+++ product_module/models.py
@@ -6,8 +6,17 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=300, verbose_name='عنوان')
+    url_title = models.CharField(max_length=300, verbose_name='عنوان در url')
+
+    def __str__(self):
+        return self.title
+
+
 class Product(models.Model):
     title = models.CharField(max_length=300)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True)
     price = models.IntegerField()
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=1)
     short_description = models.CharField(max_length=360, null=True)
