5ea8630566fcc8e947a9f178327dd0c809a0a303
rvngrd <rrvngrdd@gmail.com>
2024-05-07 18:34:47 +0330

    Finishing part 54(Registering ProductCategory)

--- product_module/admin.py
+++ product_module/admin.py
@@ -10,9 +10,10 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('title',)
     }
-    list_display = ['__str__', 'price', 'rating', 'is_active']
+    list_display = ['__str__', 'price', 'rating', 'is_active', 'category']
     list_filter = ['is_active']
     list_editable = ['is_active']
 
 
 admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.ProductCategory)
