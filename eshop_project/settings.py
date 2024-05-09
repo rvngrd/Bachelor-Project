d8c02b9f94caf830fb7a6c2f5a166ef193f51ff5
rvngrd <rrvngrdd@gmail.com>
2024-05-09 17:30:53 +0330

    Finishing part 62(Render partial, and using block and include)

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product_module',
-    'home_module'
+    'home_module',
+    'django_render_partial'
 ]
 
 MIDDLEWARE = [
