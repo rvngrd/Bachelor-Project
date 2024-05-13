c8fa828c8d99672dc2788fc7b9a4019e85cc0347
rvngrd <rrvngrdd@gmail.com>
2024-05-13 20:43:27 +0330

    Finishing part 67(Working with request method in view)

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 
 def contact_us_page(request):
+    if request.method == 'POST':
+        print(request.POST['email'])
+        return reversed(redirect('home_page'))
     return render(request, 'contact_module/contact_us_page.html')
