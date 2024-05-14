7386f375ebe7c558ea8d59ad724009257ebc8571
rvngrd <rrvngrdd@gmail.com>
2024-05-14 12:45:26 +0330

    Finishing part 70(Separate use of forms)

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -8,8 +8,36 @@ class ContactUsForm(forms.Form):
         # required=False,
         error_messages={
             'required': 'لطفا نام و نام خانوادگی خود را وارد کنید'
+        },
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'نام و نام خانوادگی'
         }
+        )
     )
-    email = forms.EmailField(label='ایمیل')
-    subject = forms.CharField(label='عنوان')
-    text = forms.CharField(label='متن پیام', widget=forms.Textarea)
+    email = forms.EmailField(
+        label='ایمیل',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'ایمیل'
+        }
+        )
+    )
+    subject = forms.CharField(
+        label='عنوان',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'عنوان'
+        }
+        )
+    )
+    text = forms.CharField(
+        label='متن پیام',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'متن پیام',
+            'rows': '5',
+            'id': 'message'
+        }
+        )
+    )
