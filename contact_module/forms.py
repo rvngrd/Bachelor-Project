2221a17e7443bc1da89bbb0d24d95ab5a12a83dd
rvngrd <rrvngrdd@gmail.com>
2024-05-14 19:59:03 +0330

    Finishing part 72(Widgets and labels in ModelForms)

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -50,3 +50,30 @@ class ContactUsModelForm(forms.ModelForm):
         fields = ['full_name', 'email', 'title', 'message']
         # fields = '__all__'
         # exclude = ['email']
+        widgets = {
+            'full_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'نام و نام خانوادگی'
+            }
+            ),
+            'email': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'ایمیل'
+            }
+            ),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'عنوان'
+            }
+            ),
+            'message': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+                'id': 'message',
+                'placeholder': 'متن پیام'
+            }
+            ),
+        }
+        labels = {
+            'message': 'متن پیام'
+        }
