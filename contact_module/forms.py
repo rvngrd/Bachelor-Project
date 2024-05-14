6e1967f235e1312055f06d6e69b58fb69a7a330b
rvngrd <rrvngrdd@gmail.com>
2024-05-14 12:10:12 +0330

    Finishing part 69(Label, error_messages and more in forms)

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField(label='نام و نام خانوادگی')
-    email = forms.EmailField()
-    subject = forms.CharField()
-    text = forms.CharField()
+    full_name = forms.CharField(
+        label='نام و نام خانوادگی',
+        max_length=50,
+        # required=False,
+        error_messages={
+            'required': 'لطفا نام و نام خانوادگی خود را وارد کنید'
+        }
+    )
+    email = forms.EmailField(label='ایمیل')
+    subject = forms.CharField(label='عنوان')
+    text = forms.CharField(label='متن پیام', widget=forms.Textarea)
--- contact_module/views.py
+++ contact_module/views.py
@@ -10,7 +10,8 @@ def contact_us_page(request):
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
             return redirect('home_page')
-    contact_form = ContactUsForm()
+    else:
+        contact_form = ContactUsForm()
     return render(request, 'contact_module/contact_us_page.html', context={
         'contact_form': contact_form
     })
