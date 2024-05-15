fe71ac33a79d6a6facf4126f3f688608a6399c29
rvngrd <rrvngrdd@gmail.com>
2024-05-15 17:27:30 +0330

    Finishing part 73(Class-based views and TemplateView)

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, redirect
+from django.views import View
+
 from .forms import ContactUsForm, ContactUsModelForm
 from .models import ContactUs
 
 # Create your views here.
+
+
+class ContactUsView(View):
+    def get(self, request):
+        contact_form = ContactUsModelForm()
+        return render(request, 'contact_module/contact_us_page.html', context={
+            'contact_form': contact_form
+        })
+
+    def post(self, request):
+        contact_form = ContactUsModelForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            return render(request, 'contact_module/contact_us_page.html', context={
+                'contact_form': contact_form
+            })
 
 
 def contact_us_page(request):
--- home_module/urls.py
+++ home_module/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name='home_page'),
-    path('contact-us', views.contact_page),
+    path('', views.HomeView.as_view(), name='home_page'),
     # path('site-header', views.site_header_partial, name='site_header_partial')
 ]
