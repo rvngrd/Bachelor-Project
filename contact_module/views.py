86fc88c43f4557582e865a12a6436cf6d2854125
rvngrd <rrvngrdd@gmail.com>
2024-05-15 20:45:10 +0330

    Finishing part 76(CreateView and introduction to work with files)

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView, View
 from .forms import ContactUsForm, ContactUsModelForm
-
+from .models import ContactUs
 
 # Create your views here.
 
 
-class ContactUsView(FormView):
+class ContactUsView(CreateView):
     template_name = 'contact_module/contact_us_page.html'
     form_class = ContactUsModelForm
     success_url = '/contact-us/'
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    #
+    # def form_valid(self, form):
+    #     form.save()
+    #     return super().form_valid(form)
 
     # def post(self, request):
     #     contact_form = ContactUsModelForm(request.POST)
@@ -22,3 +23,12 @@ class ContactUsView(FormView):
     #         return render(request, 'contact_module/contact_us_page.html', context={
     #             'contact_form': contact_form
     #         })
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'contact_module/create_profile_page.html')
+
+    def post(self, request):
+        print(request.FILES)
+        return redirect('/contact-us/create-profile')
