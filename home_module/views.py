85af0d2b2c132898b5cad4be9bb15c4ca988d0aa
rvngrd <rrvngrdd@gmail.com>
2024-05-20 23:00:13 +0330

    Finishing part 92(Dynamic slider(slider-carousel prev not working properly))

--- home_module/views.py
+++ home_module/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-from site_module.models import SiteSetting, FooterLinkBox
+from site_module.models import SiteSetting, FooterLinkBox, Slider
 
 
 # Create your views here.
 
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, 'home_module/index_page.html')
+class HomeView(TemplateView):
+    template_name = 'home_module/index_page.html'
 
-
-# class HomeView(TemplateView):
-#     template_name = 'home_module/index_page.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['data'] = 'salam!'
-#         return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sliders = Slider.objects.filter(is_active=True)
+        context['sliders'] = sliders
+        return context
 
 
 def site_header_component(request):
