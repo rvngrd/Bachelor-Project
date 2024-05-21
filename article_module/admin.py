529381939e306740d9c4cff412d475c418eafe10
rvngrd <rrvngrdd@gmail.com>
2024-05-21 21:06:59 +0330

    Finishing part 96(Save-model for authors)

--- article_module/admin.py
+++ article_module/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from .models import Article
 from . import models
 
 
@@ -11,8 +13,13 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'is_active']
+    list_display = ['title', 'slug', 'is_active', 'author']
     list_editable = ['is_active']
+
+    def save_model(self, request: HttpRequest, obj: Article, form, change):
+        if not change:
+            obj.author = request.user
+        return super().save_model(request, obj, form, change)
 
 
 admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
--- article_module/models.py
+++ article_module/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account_module.models import User
 
 # Create your models here.
 
@@ -26,6 +27,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='متن مقاله')
     is_active = models.BooleanField(default=True, verbose_name='فعال')
     selected_categories = models.ManyToManyField(ArticleCategory, verbose_name='دسته بندی ها')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='نویسنده', null=True, editable=False)
 
     def __str__(self):
         return self.title
