91d26ca8240cceb006dc03343957e8c0403fa9f6
rvngrd <rrvngrdd@gmail.com>
2024-05-31 12:47:18 +0330

    Finishing part 122(Adding product to cart(second part))

--- order_module/views.py
+++ order_module/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
+from order_module.models import Order, OrderDetail
 from product_module.models import Product
 
 
@@ -12,7 +13,21 @@ def add_product_to_order(request: HttpRequest):
     if request.user.is_authenticated:
         product = Product.objects.filter(id=product_id, is_active=True, is_delete=False).first()
         if product is not None:
-            pass
+            current_order, created = Order.objects.get_or_create(is_paid=False, user_id=request.user.id)
+            current_order_detail = current_order.orderdetail_set.filter(product_id=product_id).first()
+            if current_order_detail is not None:
+                current_order_detail.count += int(count)
+                current_order_detail.save()
+            else:
+                new_detail = OrderDetail(order_id=current_order.id, product_id=product_id, count=count)
+                new_detail.save()
+            return JsonResponse({
+                'status': 'success'
+            })
+        else:
+            return JsonResponse({
+                'status': 'not fount'
+            })
     else:
         return JsonResponse({
             'status': 'not_auth'
